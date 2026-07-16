2fc3bdf8d58da7323d8491b69efb2df816605749
nicolondonoe <nicolondonoe@gmail.com>
2026-07-16 00:28:20 -0500

    Solve excercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = ( 
+    (fahrenheit-32) * (5/9)
+  );
+
+  const roundedCelsius = Math.round(celsius * 10) / 10;
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = ( 
+    (celsius) * (9/5) + 32
+  );
+
+  const roundedFahrenheit = Math.round(fahrenheit * 10) / 10;
+  return roundedFahrenheit;
+
 };
 
 // Do not edit below this line
