cfd9eb6779a19645997d6f537c353c8207030724
nicolondonoe <nicolondonoe@gmail.com>
2026-07-16 00:03:30 -0500

    Solve excercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(numA, numB) {
+    if ((numA > 0 && numB > 0) && (Number.isInteger(numA) && Number.isInteger(numB))) {
+        let greatestNum;
+        let smallestNum;
+        if (numA > numB) {
+            greatestNum = numA;
+            smallestNum = numB;
+        } else {
+            greatestNum = numB;
+            smallestNum = numA;
+        }
+    
+        let numberArray = [];
+        let index = 0;
+        let currentNum = smallestNum;
+        for (let i = smallestNum; i <= greatestNum; i++) {
+            numberArray[index] = i;
+            index++;
+        }
+    
+        const result = numberArray.reduce( (previousValue, currentValue) => previousValue + currentValue );
+        return result;
+    }
+    else {
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
