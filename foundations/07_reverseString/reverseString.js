551f4182d839d4d245415e7249942af8c2fb989c
nicolondonoe <nicolondonoe@gmail.com>
2026-07-15 22:46:53 -0500

    Solve excercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    return stringArray
+        .reverse()
+        .join("");
 };
 
 // Do not edit below this line
