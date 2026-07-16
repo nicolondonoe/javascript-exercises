83eb897ec10eef38f17ae681971efa5e50545c1d
nicolondonoe <nicolondonoe@gmail.com>
2026-07-15 23:35:44 -0500

    Solve excercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let searchFrom = 0;
+    let indexToRemove = 0;
+    for (let i = 1; i < (arguments.length); i++) {
+        while (indexToRemove >= 0) {
+            indexToRemove = array.indexOf(arguments[i], searchFrom);
+            if (indexToRemove >= 0) {
+                array.splice(indexToRemove, 1); 
+            }
+            searchFrom = array.indexOf(arguments[i], searchFrom);
+        }
+        searchFrom = 0;
+        indexToRemove = 0;
+    }
+
+    return array;
 };
 
 // Do not edit below this line
